2d0fe5855e3b2074f4f1253c43ccd3677e264a50
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-21 06:08:45 -0600

    select box

--- pages/Builder Tool.py
+++ pages/Builder Tool.py
@@ -18,3 +18,29 @@ df = pd.read_csv(os.path.join("datasets/", st.session_state["selected_file"]))
 st.write("### Preview of data")
 st.write(df.head())
 
+st.write("---")
+
+# selcetor for type
+
+st.write("### Add Section")
+
+section_add_select = st.selectbox("Select a section type", ["Text", "Code", "Graph", "Table", "Image"])
+
+add_button = st.button("Add Section")
+
+if add_button:
+    if section_add_select == "Text":
+        st.write("Text")
+        st.write("---")
+    elif section_add_select == "Code":
+        st.write("Code")
+        st.write("---")
+    elif section_add_select == "Graph":
+        st.write("Graph")
+        st.write("---")
+    elif section_add_select == "Table":
+        st.write("Table")
+        st.write("---")
+    elif section_add_select == "Image":
+        st.write("Image")        
+        st.write("---")
