76146d3e85600d22135bc62b79686869424c5233
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-21 06:04:36 -0600

    data preview

--- pages/Builder Tool.py
+++ pages/Builder Tool.py
@@ -1,11 +1,20 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+import os
+
+import pandas as pd
 
 st.markdown("# Builder Tool")
 #st.sidebar.markdown("# Builder Tool")
 
 try :
-    st.write(st.session_state["selected_file"])
+    st.write("## " + st.session_state["selected_file"])
 except:
     switch_page("Home")
+
+# read file
+df = pd.read_csv(os.path.join("datasets/", st.session_state["selected_file"]))
+
+st.write("### Preview of data")
+st.write(df.head())
 
