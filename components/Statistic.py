13560b42b1f4e565bb401df37ab9f26e7dea1c7e
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-10-21 16:40:45 -0500

    fixing stat page

--- components/Statistic.py
+++ components/Statistic.py
@@ -1,32 +1,62 @@
 from . import component
 
 import streamlit as st
+import pandas as pd
 
 class Statistic(component.component):
     def __init__(self, df):
         super().__init__(df)
-        self.stat_type = None
-        self.run = None
-        self.output = None
+        print("Statistic init")
+        tmp = vars(self)
+        # self.stat_type = None
+        # self.run = None
+        # self.output = None
+        print(self.uuid)
+
+        if "stat_type" not in tmp:
+            self.stat_type = None
+        if "run" not in tmp:
+            self.run = None
+        if "output" not in tmp:
+            self.output = None
+        if "col1" not in tmp:
+            self.col1 = None
 
     def display(self):
+        print("Statistic display")
         st.write("### Statistic")
         #selct statistic
-        self.stat_type = st.selectbox("Select a statistic", ["Mean", "Median", "Standard Deviation", "IQR", "Percentile"])
+
+        if self.stat_type != None:
+            print("here" + self.stat_type)
+
+        with st.form(key="form_"+self.uuid, clear_on_submit=False):
+            choice = st.selectbox(
+                    "Select a statistic", ["Mean", "Median", "Standard Deviation", "IQR", "Percentile"],
+                    key="type_"+self.uuid,
+                )
         
-        st.write("### Column")
+        #st.write("### Column")
         # select column
-        col = st.selectbox("Select a column", self.df.columns, key=self.get_count())
+            column = st.selectbox("Select a column", self.df.columns, key="col1_"+self.uuid)
         
-        self.run = st.button("Run", key=self.get_count(), on_click=self.run_stat)
+            self.run = st.form_submit_button("Run")
+
+            if self.run:
+                self.run_stat()
 
         if self.output:
             self.output.display()
         
+        if choice == "Mean":
+            df2 = self.df[column].mean()
+            st.write(df2)
+
         st.write("---")
 
     def run_stat(self):
-        self.output = StatisticOutput(self.df, self.stat_type)
+        print(self.stat_type)
+        self.output = StatisticOutput(self.df, st.session_state["type_"+self.uuid], st.session_state["col1_"+self.uuid])
 
 class StatisticOutput(component.component):
     def __init__(self, df, type):
