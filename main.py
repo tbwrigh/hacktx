f414b1d5513dc50f37ac19a74e3e84e95756e6b6
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-21 05:02:42 -0600

    init stuff

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("# Welcome to Auto ML")
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
