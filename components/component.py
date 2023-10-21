f88a51da514dab1f5a8e682f6411e270229f814e
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-21 15:47:27 -0500

    uuid

--- components/component.py
+++ components/component.py
@@ -6,7 +6,7 @@ class component(ABC):
     @abstractmethod
     def __init__(self, df):
         self.df = df
-        self.count = str(uuid.uuid4())
+        self.uuid = str(uuid.uuid4())
     @abstractmethod
     def display(self):
         pass
